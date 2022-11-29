2b2bd6b26a9ccea3722d1a4e69953570963dc231
V Chandra Shekhar <vcs200831@gmail.com>
2022-11-29 19:24:06 +0530

    Time: 1311 ms (23.96%), Space: 61.7 MB (7.29%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,39 +1,37 @@
 class RandomizedSet:
 
     def __init__(self):
-        # Store the index of each val in self.arr.
-        self.indices = {}
-        # Store all vals.
-        self.arr = []
+        self.numMap={}
+        self.numList=[]
+        
 
     def insert(self, val: int) -> bool:
-        # Return False if val is already present as requested.
-        if val in self.indices: return False
+        result = val not in self.numMap
+        if  result:
+            self.numMap[val] = len(self.numList)
+            self.numList.append(val)
+        return result
         
-        # Append val to the array.
-        # Store its index in the hashmap
-        self.arr.append(val)
-        self.indices[val] = len(self.arr)-1
-        return True
-    
+
     def remove(self, val: int) -> bool:
-        # Return False if val is not present as requested.
-        if val not in self.indices: return False
+        result = val in self.numMap
+        if result:
+            index = self.numMap[val]
+            lastVal = self.numList[-1]
+            self.numList[index] = lastVal
+            self.numList.pop()
+            self.numMap[lastVal]=index
+            del self.numMap[val]
+        return result
         
-        # Get the index of the val that needs to be removed.
-        i = self.indices[val]
         
-        # Update the index of arr[-1] in the indices.
-        self.indices[self.arr[-1]] = i
-        
-        # Move the last element to the i th position.
-        self.arr[i] = self.arr[-1]
-        
-        # remove the last element, and remove the index of val
-        self.indices.pop(val)
-        self.arr.pop()
-        
-        return True
 
     def getRandom(self) -> int:
-        return random.choice(self.arr)
+        return random.choice(self.numList)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
