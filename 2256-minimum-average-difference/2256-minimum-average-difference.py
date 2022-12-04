24e29d350e0ce994d29c71989c8b11ce96f02cdc
V Chandra Shekhar <vcs200831@gmail.com>
2022-12-04 12:12:52 +0530

    Time: 2887 ms (23.20%), Space: 24.9 MB (80.12%) - LeetHub

---
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumAverageDifference(self, nums: List[int]) -> int:
+        n, totalSum, leftSum = len(nums), sum(nums), 0
+        
+        # res[0] is the minimum absolute difference, and res[1] is the index.
+        res = [inf,inf]
+        
+        for i,v in enumerate(nums):
+            leftSum += v
+            
+            leftAvg = leftSum//(i+1)
+            # when we are at the last index, the right hand side sum is 0. (To aviod divide by zero error)
+            rightAvg = (totalSum-leftSum)//(n-i-1) if n-i-1!=0 else 0
+            absDif = abs(leftAvg-rightAvg)
+            
+            # min will take care of "If there are multiple such indices, return the smallest one."
+            res = min(res,[absDif,i])
+        return res[1]
